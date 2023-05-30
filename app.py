36888d3548ed6f64ea45e5a278628b5856c850c8
hiddevr <hiddevrooijen@hotmail.nl>
2023-05-30 13:59:10 +0200

    jh

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, render_template
+from google.cloud import storage
+import cv2
+import requests
+import numpy as np
+import uuid
+import os
+import tempfile
+
+app = Flask(__name__)
+
+
+def process_file_or_url(file_obj, url):
+    if file_obj is not None:
+        # Save the file to a temporary file
+        fd, filename = tempfile.mkstemp()
+        with os.fdopen(fd, 'wb') as tmp:
+            tmp.write(file_obj.read())
+    elif url is not None:
+        # Download the file from the URL to a temporary file
+        response = requests.get(url, stream=True)
+        fd, filename = tempfile.mkstemp()
+        with os.fdopen(fd, 'wb') as tmp:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    tmp.write(chunk)
+    else:
+        filename = None
+    return filename
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_files():
+    if request.method == 'GET':
+        # Serve the HTML frontend
+        return render_template('upload.html')
+    elif request.method == 'POST':
+        user_id = uuid.uuid4()
+
+        # Process the video file
+        video_file = request.files.get('video-file')
+        video_url = request.form.get('video-url')
+        video_filename = process_file_or_url(video_file, video_url)
+
+        # Process the image file
+        image_file = request.files.get('image-file')
+        image_url = request.form.get('image-url')
+        image_filename = process_file_or_url(image_file, image_url)
+
+        # Open video file with OpenCV
+        vidcap = cv2.VideoCapture(video_filename)
+        success, image_frame = vidcap.read()
+        frames = []
+        while success:
+            frames.append(image_frame)
+            success, image_frame = vidcap.read()
+        vidcap.release()
+
+        # Upload watermark image to Google Cloud Storage
+        storage_client = storage.Client.from_service_account_json('cc-assigment2-388310-9c5a6c31333e.json')
+        bucket = storage_client.get_bucket('worker-data-store')
+        blob = bucket.blob(f'{user_id}/watermark.png')
+        with open(image_filename, 'rb') as img_file:
+            blob.upload_from_file(img_file)
+
+        # Then upload each frame to Google Cloud Storage
+        for i, frame in enumerate(frames):
+            _, encoded_image = cv2.imencode('.png', frame)
+            blob = bucket.blob(f'{user_id}/frame{i}.png')
+            blob.upload_from_string(encoded_image.tostring(), content_type='image/png')
+
+        os.remove(video_filename)
+        os.remove(image_filename)
+        return 'Done'
