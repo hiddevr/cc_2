cd628cd01b5781d0f6bb49e005dd785ddce44e92
hiddevr <hiddevrooijen@hotmail.nl>
2023-06-01 11:09:18 +0200

    asdasdsa

--- watermark-worker/watermark_worker.py
+++ watermark-worker/watermark_worker.py
@@ -90,7 +90,7 @@ def index():
 
         # Update Firestore
         doc_ref = db.collection('jobs').document(video_id)
-        transaction = db.transaction
+        transaction = db.transaction()
         finished = process_frame(transaction, doc_ref, frame_number)
 
         # If all frames are processed, publish a message to the 'reduce-video' topic
