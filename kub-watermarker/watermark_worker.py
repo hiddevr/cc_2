7a084a171599117fdcc0f20b5ccd9d3b9b6d79a6
hiddevr <hiddevrooijen@hotmail.nl>
2023-06-15 13:41:33 +0200

    asdasdsa

--- kub-watermarker/watermark_worker.py
+++ kub-watermarker/watermark_worker.py
@@ -24,54 +24,58 @@ def process_frame(transaction, doc_ref, frame_number):
 
 def callback(message):
     # Decode the Pub/Sub message
-    data = base64.b64decode(message.data).decode('utf-8')
-    data = json.loads(data)
-    video_id = data['video_id']
-    frame_number = data['frame_number']
+    try:
+        data = base64.b64decode(message.data).decode('utf-8')
+        data = json.loads(data)
+        video_id = data['video_id']
+        frame_number = data['frame_number']
 
-    # Fetch the watermark.png and specific frame
-    watermark_bucket = storage_client.get_bucket('full-vid-storage')
-    watermark_blob = watermark_bucket.blob(f'{video_id}/watermark.png')
-    watermark = Image.open(io.BytesIO(watermark_blob.download_as_bytes()))
+        # Fetch the watermark.png and specific frame
+        watermark_bucket = storage_client.get_bucket('full-vid-storage')
+        watermark_blob = watermark_bucket.blob(f'{video_id}/watermark.png')
+        watermark = Image.open(io.BytesIO(watermark_blob.download_as_bytes()))
 
-    frame_bucket = storage_client.get_bucket('worker-data-storage')
-    frame_blob = frame_bucket.blob(f'{video_id}/frame{frame_number}.png')
-    frame = Image.open(io.BytesIO(frame_blob.download_as_bytes()))
+        frame_bucket = storage_client.get_bucket('worker-data-storage')
+        frame_blob = frame_bucket.blob(f'{video_id}/frame{frame_number}.png')
+        frame = Image.open(io.BytesIO(frame_blob.download_as_bytes()))
 
-    # Check if the frame is already processed
-    output_bucket = storage_client.get_bucket('processed-frames')
-    output_blob = output_bucket.blob(f'{video_id}/frame{frame_number}.png')
+        # Check if the frame is already processed
+        output_bucket = storage_client.get_bucket('processed-frames')
+        output_blob = output_bucket.blob(f'{video_id}/frame{frame_number}.png')
 
-    frame.paste(watermark, (0, 0), watermark)
+        frame.paste(watermark, (0, 0), watermark)
 
-    # Save image to BytesIO object
-    byte_arr = io.BytesIO()
-    frame.save(byte_arr, format='PNG')
-    byte_arr.seek(0) # reset the pointer to the start of BytesIO object
+        # Save image to BytesIO object
+        byte_arr = io.BytesIO()
+        frame.save(byte_arr, format='PNG')
+        byte_arr.seek(0)  # reset the pointer to the start of BytesIO object
 
-    # Upload BytesIO object to GCS
-    output_blob.upload_from_file(byte_arr, content_type='image/png')
+        # Upload BytesIO object to GCS
+        output_blob.upload_from_file(byte_arr, content_type='image/png')
 
-    # Update Firestore
-    doc_ref = db.collection('jobs').document(video_id)
-    transaction = db.transaction()
-    process_frame(transaction, doc_ref, frame_number)
+        # Update Firestore
+        doc_ref = db.collection('jobs').document(video_id)
+        transaction = db.transaction()
+        process_frame(transaction, doc_ref, frame_number)
 
-    doc_ref = db.collection('jobs').document(video_id)
-    processed_dict = doc_ref.get().to_dict().get('processed')
-    completed = doc_ref.get().to_dict().get('completed')
-    if all(processed_dict.values()) and not completed:
-        finished = True
-    else:
-        finished = False
+        doc_ref = db.collection('jobs').document(video_id)
+        processed_dict = doc_ref.get().to_dict().get('processed')
+        completed = doc_ref.get().to_dict().get('completed')
+        if all(processed_dict.values()) and not completed:
+            finished = True
+        else:
+            finished = False
 
-    # If all frames are processed, publish a message to the 'reduce-video' topic
-    if finished:
-        topic_path = publisher.topic_path(PROJECT_ID, 'reduce-video')
-        message_json = json.dumps({'video_id': video_id}).encode('utf-8')
-        publisher.publish(topic_path, data=message_json)
+        # If all frames are processed, publish a message to the 'reduce-video' topic
+        if finished:
+            topic_path = publisher.topic_path(PROJECT_ID, 'reduce-video')
+            message_json = json.dumps({'video_id': video_id}).encode('utf-8')
+            publisher.publish(topic_path, data=message_json)
 
-    message.ack()
+        message.ack()
+    except Exception as e:
+        print(f"An exception occurred in process_message: {e}")
+        raise
 
 if __name__ == "__main__":
     # Subscribe to the subscription
